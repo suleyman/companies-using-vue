7cd3a19e65bb9cb3163cb9538b570104004ecde7
suleymandtr <slymnd28@gmail.com>
2021-05-15 22:58:43 +0300

    Updated: filters

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,51 @@
 <template>
-  <div class="bg-gray-100">
+  <div class="bg-gray-100 pt-12 min-h-screen">
     <div class="container mx-auto">
-      <section class="pt-12 md:px-0 px-12 grid md:grid-cols-4 grid-cols-1 gap-8">
-        <Company v-for="(company, index) in companies" :company-data="company" />
+      <section class="p-12 bg-white rounded-3xl">
+        <form class="flex items-center">
+          <div class="mr-12">
+            <input id="isRemote" type="checkbox" v-model="isRemote" />
+            <label for="isRemote">Remote</label>
+          </div>
+          <div>
+            <label for="location">Location</label>
+            <div class="relative inline-block text-gray-700">
+              <select
+                class="w-full h-10 pl-3 pr-6 text-base placeholder-gray-600 border rounded-lg appearance-none focus:shadow-outline"
+                v-model="selectedLocation"
+              >
+                <option :value="null">All</option>
+                <option v-for="location in companyLocations" :value="location">{{ location }}</option>
+              </select>
+              <div class="absolute inset-y-0 right-0 flex items-center px-2 pointer-events-none">
+                <svg class="w-4 h-4 fill-current" viewBox="0 0 20 20">
+                  <path
+                    d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
+                    clip-rule="evenodd"
+                    fill-rule="evenodd"
+                  ></path>
+                </svg>
+              </div>
+            </div>
+          </div>
+        </form>
       </section>
+      <transition>
+        <section class="pt-12 md:px-0 px-12 grid md:grid-cols-4 grid-cols-1 gap-8">
+          <Company v-for="(company, index) in filteredCompanies" :company-data="company" />
+        </section>
+      </transition>
     </div>
   </div>
 </template>
 
 <script lang="ts">
-import { defineComponent, ref } from "vue";
+import { defineComponent, ref, computed, Ref } from "vue";
+import { ICompany } from "./interfaces/company";
 import Company from "./components/Company.vue";
 import companiesData from "./data/companies.json";
-import { ICompany } from "./interfaces/company";
+import _filter from "lodash/filter";
+import _uniq from "lodash/uniq";
 
 export default defineComponent({
   name: "App",
@@ -20,11 +53,26 @@ export default defineComponent({
     Company,
   },
   setup: () => {
-    let companies = ref<Array<ICompany>>([]);
-    for (let i = 0; i < 24; i++) {
-      companies.value.push(companiesData[0]);
-    }
-    return { companies };
+    const companies: Ref<ICompany[]> = ref(companiesData);
+    const companyLocations = _uniq(companies.value.map((company: ICompany) => company.location));
+
+    const selectedLocation: Ref<string | null> = ref(null);
+    const isRemote: Ref<boolean> = ref(true);
+
+    const filteredCompanies = computed(() => {
+      return _filter(companies.value, {
+        isRemote: isRemote.value,
+        ...(selectedLocation.value ? { location: selectedLocation.value } : {}),
+      });
+    });
+
+    return {
+      companies,
+      companyLocations: new Set(companyLocations),
+      selectedLocation,
+      isRemote,
+      filteredCompanies,
+    };
   },
 });
 </script>
