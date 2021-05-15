58e80c52f7ad89942dfa5dbf730ec2889ddd209c
suleymandtr <slymnd28@gmail.com>
2021-05-15 23:35:33 +0300

    add: animation

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,27 @@
 module.exports = {
-  purge: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
+  purge: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      keyframes: {
+        "fade-in-up": {
+          "0%": {
+            opacity: "0",
+            transform: "translateY(10px)",
+          },
+          "100%": {
+            opacity: "1",
+            transform: "translateY(0)",
+          },
+        },
+      },
+      animation: {
+        "fade-in-up": "fade-in-up 0.5s ease-out",
+      },
+    },
   },
   variants: {
     extend: {},
   },
   plugins: [],
-}
+};
